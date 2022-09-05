1f0657dc5ce05a761325c152e0bb954ec74f72dc
AdetunjiJakes <68664742+Jamleads@users.noreply.github.com>
2022-09-05 14:31:46 +0100

    expressflight dummy flight booking landing page

---
+++ script.js
@@ -0,0 +1,27 @@
+// const swiper = new Swiper('.swiper', {
+//     // Optional parameters
+//     direction: 'horizontal',
+//     loop: true,
+  
+//     // If we need pagination
+//     pagination: {
+//       el: '.swiper-pagination',
+//     },
+  
+//     // Navigation arrows
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev',
+//     },
+  
+//     // And if we need scrollbar
+//     scrollbar: {
+//       el: '.swiper-scrollbar',
+//     },
+
+//     // this shows a bit of the previous/next slides
+//     slidesPerView: 1.2,
+//     centeredSlides: true,
+//     spaceBetween: 10,
+    
+// });
